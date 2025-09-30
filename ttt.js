d435cf72c29c3c68a56ec4768f55d8cc8e874743
Abhishek vishwakarma <abhijnv2004@gmail.com>
2025-09-30 18:42:46 +0530

    Add files via upload

---
+++ ttt.js
@@ -0,0 +1,88 @@
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let newgameBtn=document.querySelector("#new-btn ");
+let msgcontainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+
+let turno=true;
+
+const winpatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+[2,4,6],
+[3,4,5],
+[6,7,8],
+
+];
+const resetgame = ()=>{
+    turno=true;
+    enableBoxes();
+    msgcontainer.classList.add("hide");
+}
+
+
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("box was cliked");
+        if(turno){
+            box.innerText="o";
+            turno=false;
+
+        }else{
+            box.innerText="x";
+            turno=true;
+
+        }
+        box.disabled=true;
+
+        checkWinner();
+        
+    });
+});
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+
+const showWinner=(winner)=>{
+    msg.innerText = `congratulation,winner is ${winner}`;
+msgcontainer.classList.remove("hide");
+disableBoxes();
+}
+
+
+const checkWinner=()=>{
+    for(let pattern of winpatterns){
+       let pos1Val=boxes[pattern[0]].innerText;
+       let pos2Val=boxes[pattern[1]].innerText;
+       let pos3Val=boxes[pattern[2]].innerText;
+        if(pos1Val !=""&& pos2Val!="" && pos3Val!="" ){
+if(pos1Val===pos2Val && pos2Val===pos3Val){
+    console.log("winner",pos1Val);
+    showWinner(pos1Val);
+}
+        }
+
+       
+    }
+};
+
+resetBtn.addEventListener("click",resetgame);
+newgameBtn.addEventListener("click",resetgame);
+
+
+ 
